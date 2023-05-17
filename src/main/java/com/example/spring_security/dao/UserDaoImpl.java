9bf79becd47b19e307ad89f44395c0c2ea2ad58a
Artem <ivann3692gmail.com>
2023-05-17 11:35:49 +0300

    spf

---
+++ src/main/java/com/example/spring_security/dao/UserDaoImpl.java
@@ -0,0 +1,101 @@
+package com.example.spring_security.dao;
+
+import com.example.spring_security.entity.Role;
+import com.example.spring_security.entity.User;
+import javax.persistence.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Repository
+public class UserDaoImpl implements UserDao{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    public UserDaoImpl(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return entityManager.createQuery("select u from User u", User.class).getResultList();
+    }
+
+    @Override
+    public Set<Role> getAllRoles() {
+        return new HashSet<>(
+                entityManager.createQuery("select r from Role r", Role.class).getResultList()
+        );
+    }
+
+    @Override
+    public Role getRoleByName(String name){
+        TypedQuery<Role> roleName = entityManager.createQuery("select r from Role r where r.name = :name", Role.class);
+        roleName.setParameter("name", name);
+        return roleName.getResultList().stream().findFirst().orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void saveUser(User user) {
+        User newUser = new User();
+        newUser.setAge(user.getAge());
+        newUser.setEnabled(user.isEnabled());
+        newUser.setFirstName(user.getFirstName());
+        newUser.setLastName(user.getLastName());
+        newUser.setLogin(user.getLogin());
+
+        if (user.getRoles().isEmpty()){
+            newUser.addRole(getRoleByName("ROLE_USER"));
+        } else {
+            Set<Role> roles = user.getRoles();
+            for (Role roleInSet : roles) {
+                newUser.addRole(getRoleByName(roleInSet.getName()));
+            }
+        }
+
+        if (user.getId() == null) {
+            newUser.setPassw(bCryptPasswordEncoder.encode(user.getPassword()));
+            entityManager.persist(newUser);
+        } else {
+            newUser.setId(user.getId());
+
+            if (user.getPassword()==null) {
+                newUser.setPassw(getSingleUserById(user.getId()).getPassword());
+            } else {
+                newUser.setPassw(bCryptPasswordEncoder.encode(user.getPassword()));
+            }
+            entityManager.merge(newUser);
+        }
+    }
+
+    @Override
+    public User getSingleUserById(Long id) {
+        TypedQuery<User> typedQuery = entityManager.createQuery("select u from User u where u.id = :id", User.class);
+        typedQuery.setParameter("id", id);
+        User user = typedQuery.getResultList().stream().findFirst().orElse(null);
+        user.setPassw(null);
+        return user;
+    }
+
+    @Override
+    public User getSingleUserByLogin(String login) {
+        TypedQuery<User> typedQuery = entityManager.createQuery("select u from User u where u.login = :login", User.class);
+        typedQuery.setParameter("login", login);
+        return typedQuery.getResultList().stream().findFirst().orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void deleteUser(Long id) {
+        entityManager.remove(getSingleUserById(id));
+    }
+}
---
+++ src/main/java/com/example/spring_security/entity/User.java
@@ -0,0 +1,175 @@
+package com.example.spring_security.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+@Table(name = "users123", uniqueConstraints = @UniqueConstraint(columnNames = {"login"}))
+public class User implements UserDetails{
+
+//поля
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "age")  
+    private Integer age;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "passw")
+    private String passw;
+
+    @Column(name = "enabled")
+    private boolean enabled;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users123_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    public void addRole(Role role) {
+        this.roles.add(role);
+    }
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, Integer age, String login, String password, boolean enabled, Set<Role> roles) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.login = login;
+        this.passw = password;
+        this.enabled = enabled;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassw() {
+        return passw;
+    }
+
+    public void setPassw(String password) {
+        this.passw = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", login='" + login + '\'' +
+                ", enabled=" + enabled +
+                ", roles=" + roles +
+                '}';
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<Role> roles = getRoles();
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        for (Role roleFromSet : roles) {
+            authorities.add(new SimpleGrantedAuthority(roleFromSet.getAuthority()));
+        }
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return getPassw();
+    }
+
+    @Override
+    public String getUsername() {
+        return getLogin();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+
+}
